2941c29d7a22f1647d68206648ad78c5a0fab5aa
Nibrasd <muostafa55@gmail.com>
2026-04-15 18:31:03 +0200

    Flatten structure to root: Guaranteed fix for ESM package resolution on Render and rebranding completed

--- server.js
+++ server.js
@@ -102,9 +102,9 @@ app.post('/api/hsp/webhook', (req, res) => {
 });
 
 // Serve Frontend Static Files (Production)
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 });
 
 app.listen(PORT, () => {
